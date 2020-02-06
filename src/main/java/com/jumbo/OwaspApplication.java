b870bf567e2e3e8bde448ebbdbc36ca6c532eef5
Oskar Albers <oalbers@wundermancommerce.com>
2020-02-06 14:50:40 +0100

    first commit

---
+++ src/main/java/com/jumbo/OwaspApplication.java
@@ -0,0 +1,11 @@
+package com.jumbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwaspApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OwaspApplication.class, args);
+    }
+}
