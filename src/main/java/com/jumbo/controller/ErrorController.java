13f00d0164c29f681ce5501373186513919cfa83
Oskar Albers <oalbers@wundermancommerce.com>
2020-02-21 14:35:02 +0100

    XXE issue

--- src/main/java/com/jumbo/controller/ErrorController.java
+++ src/main/java/com/jumbo/controller/ErrorController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.net.Socket;
+
 @RestController
 public class ErrorController {
 
@@ -23,5 +27,15 @@ public class ErrorController {
         }
 
         return data * 10;
+    }
+
+    public void test(Socket socket){
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.parse(socket.getInputStream()); //unsafe
+        }catch (Exception e){
+
+        }
     }
 }
